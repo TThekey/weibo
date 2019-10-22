bb0d437fcb8820a7fb776a7edfac7f2581beeb43
TThekey <tthekey1997@163.com>
2019-10-22 17:04:19 +0000

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -27,7 +27,6 @@ class SessionsController extends Controller
             'password' => 'required'
         ]);
 
-
         //进行身份认证
         if(Auth::attempt($credentials)){
             session()->flash('success', '欢迎回来');
@@ -36,8 +35,16 @@ class SessionsController extends Controller
             session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
             return redirect()->back()->withInput();
         }
+    }
 
 
-
+    /**
+     * 退出登录
+     */
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出');
+        return redirect('login');
     }
 }
